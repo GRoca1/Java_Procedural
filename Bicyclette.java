d8f59fcccbc25a9e983ba1d0884f34220acae2fa
Georges Roca (Joe) <76114361+GRoca1@users.noreply.github.com>
2021-05-11 10:54:44 +0200

    Update Bicyclette.java

--- Bicyclette.java
+++ Bicyclette.java
@@ -15,9 +15,9 @@ public class Bicyclette {
 
 		if (reponse.equals("oui")) {
 
-			System.out.println("J'irai faire une balade ‡ bicyclette");
+			System.out.println("J'irai faire une balade √† bicyclette");
 
-			System.out.println("Ma bicyclette est elle en Èt‚t de fonctionnement?");
+			System.out.println("Ma bicyclette est elle en √©t√¢t de fonctionnement?");
 			reponse = sc.nextLine().toLowerCase();
 
 			System.out.println(reponse);
@@ -25,28 +25,28 @@ public class Bicyclette {
 			if (reponse.equals("non")) {
 				System.out.println("je passe chez le garagiste");
 			} else
-				System.out.println("j'irais jusqu'‡ l'etang cueillir les joncs");
+				System.out.println("j'irais jusqu'√† l'etang cueillir les joncs");
 
-			System.out.println("les rÈparations seront elles immÈdiates?");
+			System.out.println("les r√©parations seront elles imm√©diates?");
 			reponse = sc.nextLine().toLowerCase();
 
 			System.out.println(reponse);
 
 			if (reponse.equals("oui")) {
-				System.out.println("je pedalerais jusqu'‡ l'etang cueillir les joncs");
+				System.out.println("je pedalerais jusqu'√† l'etang cueillir les joncs");
 			} else
-				System.out.println("j'irais ‡ pied jusqu'‡ l'etang cueillir les joncs");
+				System.out.println("j'irais √† pied jusqu'√† l'etang cueillir les joncs");
 
 		} else {
-			System.out.println("je consacre ma journÈe ‡ la lecture");
+			System.out.println("je consacre ma journ√©e √† la lecture");
 
-			System.out.println("est-ce que je possËde le 1er tome de Games of Thrones?");
+			System.out.println("est-ce que je poss√®de le 1er tome de Games of Thrones?");
 			reponse = sc.nextLine().toLowerCase();
 
 			if (reponse.equals("oui")) {
 				System.out.println("je m'installerai confortablement dans un fauteuil pour le lire");
 			} else {
-				System.out.println("j'irais l'emprunter ‡ la bibliothËque");
+				System.out.println("j'irais l'emprunter √† la biblioth√®que");
 			
 				System.out.println("Est-ce que je peux emprunter G.O.T?");
 				reponse = sc.nextLine().toLowerCase();
@@ -55,11 +55,10 @@ public class Bicyclette {
 				
 				if (reponse.equals("oui")) 
 				{
-					System.out.println("je rentre directement ‡ la maison");
+					System.out.println("je rentre directement √† la maison");
 				} else 
 				{
-					System.out.println("je vais emprunter un roman policier et je rentre directement ‡ la maison"
-							+ "");
+					System.out.println("je vais emprunter un roman policier et je rentre directement √† la maison");
 				}
 			
 
