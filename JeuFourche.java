31f08a9ab84528d569f22be598e5ea2fb79c2332
Georges Roca (Joe) <76114361+GRoca1@users.noreply.github.com>
2021-05-18 11:32:46 +0200

    Add files via upload

---
+++ JeuFourche.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class JeuFourche {
+
+	public static void main(String[] args) {
+		
+		
+		    Scanner sc = new Scanner(System.in);		   	    
+		    int compteur = 0;
+		    int min, max, nbUser, nbMachine;
+		    boolean continuer = false;
+		    Random aleas = new Random();
+		    nbMachine = aleas.nextInt(101);
+		    min = 0;
+		    max = 100;
+		    do {
+		    System.out.println("Choisissez un nombre entre " + min + " et " + max);
+		    System.out.println("Vous avez 5 essais " );
+		    nbUser=sc.nextInt();		    
+		    if (nbUser > nbMachine) {
+		    	
+		    	max=nbUser;
+		    	compteur++;
+		    	
+		    	
+		    }
+		    
+		    else if (nbUser<nbMachine)
+		    {
+		    	min=nbUser;
+		    	compteur++;
+		    }
+		    else {
+		    	compteur++;		    
+		    	continuer = true;
+		    }
+		    }
+		    while(continuer==false && compteur<5);
+		    
+		    if(continuer) {
+		    	System.out.println("Bravo ! vous avez réussi en:" +compteur+ "coups");
+		    }
+		    else {
+		    	System.out.println("Perdu! Essayez encore! ");
+		    	System.out.println("le nombre à choisir est:" + nbMachine);
+		    	sc.close();
+		    }
+		    
+	
+
+}}
