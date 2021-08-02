4ed77169c7737c80ba18e046c2da9077740413d3
Georges Roca (Joe) <76114361+GRoca1@users.noreply.github.com>
2021-08-02 14:34:39 +0200

    Add files via upload

---
+++ lib_kiloutou/ComparerNumSerie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lib_kiloutou
+{
+   public class ComparerNumSerie : IComparer<Voiture>
+    {
+        public int Compare(Voiture x, Voiture y)
+        {
+            return x.Numeroserie.CompareTo(y.Numeroserie);
+        }
+    }
+}
+
